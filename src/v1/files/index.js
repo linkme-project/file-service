86ef32e741df5750bffd7e45eef7c96cecc707bc
Keunhak Lim <limkeunhak@gmail.com>
2019-06-30 17:40:25 +0900

    handle file upload error

--- src/v1/files/index.js
+++ src/v1/files/index.js
@@ -3,6 +3,13 @@ require('dotenv').config();
 const Router = require('koa-router');
 const multer = require('koa-multer');
 
+const RESULT_CODE = {
+  SUCCESS: 0,
+  DATA_EMPTY: 1,
+  FAIL: -1,
+  API_CALL_ERROR: -2
+};
+
 // load environment variables.
 const {
   UPLOAD_PATH: uploadPath,
@@ -27,31 +34,52 @@ const storage = multer.diskStorage({
 let upload = multer({
   storage: storage,
   limits: { fileSize: 5 * 1024 * 1024 } // 5MB
-});
+}).any();
 
 // api implementations.
 const fileApi = new Router();
 
-fileApi.post('/upload', upload.any(), (ctx) => {
-  let file = ctx.req.files[0];
+fileApi.post('/upload', async (ctx, next) => {
+  try {
+    await upload(ctx, next);
+    
+    // TODO: save file info. to mongo db
+    
+    let file = ctx.req.files[0];
+
+    let result = {
+      resultCode: RESULT_CODE.SUCCESS,
+      resultMessage: null,
+      resultValue: {
+        fileName: file.originalname,
+        fileSize: file.size,        
+        fileSn: 1,
+        fileUrl: `${ctx.host}${downloadUrlPrefix}/${file.uploadedFileName}`
+      }
+    };
+  
+    ctx.body = result;    
+  } catch(ex) {
+    ctx.body = { resultCode: RESULT_CODE.FAIL,  resultMessage: ex.message,  resultValue: null };
+  }
+});
+
+fileApi.get('/:fileSn', ctx => {
+
+  // TODO: get file info. from mongo db.
 
   let result = {
     resultCode: 1,
     resultMessage: null,
     resultValue: {
-      fileName: file.originalname,
-      fileSize: file.size,        
+      fileName: '123',
+      fileSize: '123',        
       fileSn: 1,
-      fileUrl: `${ctx.host}${downloadUrlPrefix}/${file.uploadedFileName}`
+      fileUrl: '123'
     }
   };
 
   ctx.body = result;
-});
-
-fileApi.get('/:fileSn', upload.any(), (ctx) => {
-  // TODO: get file info. from mongo db.
-  ctx.body = ctx.params.fileSn;
 });
 
 module.exports = fileApi;
